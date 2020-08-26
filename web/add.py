4229f4b6a1ff0def45bb65b8f5b8f65d19cb2d74
JoseDRamirezM <eskatejose@hotmail.com>
2020-08-26 16:50:47 -0500

    funciona. juego 3

--- web/add.py
+++ web/add.py
@@ -6,4 +6,20 @@ eel.init("web")
 def add(dato1):
     return int(dato1) + 1
 
+@eel.expose
+def mas_jugado(lista):
+  return indice(maximo(lista),lista)[0]
+
+def indice(max_val, lista):
+  return [("juego" + str(i+1)) for i,num in enumerate(lista) if max_val==num ]
+
+def maximo(lista):
+	if len(lista)==1:
+		return lista[0]
+	elif lista[0] > lista[1]:
+		return maximo([lista[0]]+lista[2:])
+	else:
+		return maximo(lista[1:])
+
+
 eel.start("index.html")
--- web/main.js
+++ web/main.js
@@ -10,17 +10,32 @@ function click_juego2(){
 
 function click_juego3(){
     var n = document.getElementById("num3").value
-    eel.add(n)(callBack3)
+    eel.add(n)(callBack3)   
+}
+
+function mas_jugado(){
+    var n1 = document.getElementById("num1").value
+    var n2 = document.getElementById("num2").value
+    var n3 = document.getElementById("num3").value
+
+    eel.mas_jugado([n1,n2,n3])(callBack)
+}
+
+function callBack(result){
+    document.getElementById("masJugado").innerHTML = "JUEGO MAS JUGADO: " + result;
 }
 
 function callBack1(result){ 
     document.getElementById("num1").value = result 
+    mas_jugado()
 }
 
 function callBack2(result){ 
     document.getElementById("num2").value = result 
+    mas_jugado()
 }
 
 function callBack3(result){ 
     document.getElementById("num3").value = result 
+    mas_jugado()
 }
