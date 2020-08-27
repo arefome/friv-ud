97d54d43155e9efb23ddf77d756352793ad70c37
arefome <55114188+arefome@users.noreply.github.com>
2020-08-26 19:21:14 -0500

    Update add.py

--- web/add.py
+++ web/add.py
@@ -8,18 +8,18 @@ def add(dato1):
 
 @eel.expose
 def mas_jugado(lista):
-  return indice(maximo(lista),lista)[0]
+    return indice(maximo(lista),lista)[0]
 
 def indice(max_val, lista):
-  return [("juego" + str(i+1)) for i,num in enumerate(lista) if max_val==num ]
+    return [("juego" + str(i+1)) for i,num in enumerate(lista) if max_val==num ]
 
 def maximo(lista):
-	if len(lista)==1:
-		return lista[0]
-	elif lista[0] > lista[1]:
-		return maximo([lista[0]]+lista[2:])
-	else:
-		return maximo(lista[1:])
+    if len(lista)==1:
+        return lista[0]
+    elif lista[0] > lista[1]:
+	return maximo([lista[0]]+lista[2:])
+    else:
+	return maximo(lista[1:])
 
 
 eel.start("index.html")
